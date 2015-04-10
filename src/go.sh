f7183400edf423ffa64a450d5858306b8d908578
ffavaro <federicofsv@gmail.com>
2015-04-10 17:59:48 -0300

    saco bash del juego

--- src/go.sh
+++ src/go.sh
@@ -1,10 +1,10 @@
-#!/bin/bash -e
+#!/bin/sh -e
 
 # Make sure only root can run our script
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
-fi
+#if [[ $EUID -ne 0 ]]; then
+#   echo "This script must be run as root" 1>&2
+#   exit 1
+#fi
 
 cd build/main
 make
