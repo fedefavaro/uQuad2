fb4c8c72f5c9a4fd6ac24524ffab800010373470
ffavaro <federicofsv@gmail.com>
2015-05-27 21:24:15 -0300

    logs

--- logs/DEBUG/debug.m
+++ logs/DEBUG/debug.m
@@ -2,17 +2,52 @@ close all
 clear all
 clc
 
-data = importdata('log_debug_7.txt',' ');
+data = importdata('log_debug_11.txt',' ');
 
 t = data(:,1)*1000 + data(:,2)/1000; %timepo en milisegundos
 
-t2 = data(:,6)*1000 + data(:,7)/1000; %timepo en milisegundos
+t2 = data(:,11)*1000 + data(:,12)/1000; %timepo en milisegundos
+
+figure
+hold on
+plot(t,data(:,3),'*r')
+plot(t,data(:,4),'*b')
+plot(t,data(:,5),'*g')
+title('Euler angles')
+xlabel('time (ms)')
+legend('roll(deg)','pitch(deg)','yaw(deg)')
+grid on
+hold off
+
+figure
+plot(t,data(:,6),'*r')
+title('Yaw punto')
+xlabel('time (ms)')
+grid on
 
 figure
 plot(diff(t))
+title('Derivada del tiempo de muestreo')
+xlabel('numero de muestra')
+ylabel('tiempo(ms)')
 grid on
 
 
 figure
+hold on
+plot(t,data(:,7),'-r')
+plot(t,data(:,8),'-b')
+plot(t,data(:,9),'-g')
+plot(t,data(:,10),'-k')
+title('Comandos')
+xlabel('time (ms)')
+legend('Roll','Pitch','Yaw','Throttle')
+grid on
+hold off
+
+figure
 plot(diff(t2))
+title('Derivada del tiempo de loop')
+xlabel('numero de muestra')
+ylabel('tiempo(ms)')
 grid on
