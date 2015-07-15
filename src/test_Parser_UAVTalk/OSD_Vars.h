a1df7ac3c6b2d28f48d1facf13ec6e465abb63bd
ffavaro <federicofsv@gmail.com>
2015-07-15 19:36:24 -0300

    parser uavtalk

---
+++ src/test_Parser_UAVTalk/OSD_Vars.h
@@ -0,0 +1,20 @@
+//
+//
+//
+
+#include <stdbool.h>
+#include <stdint.h>
+
+/*static uint8_t      osd_mode = 0;                   // FlightMode of the FlightControl
+static float        osd_heading = 0;                // ground course heading
+static int16_t      osd_roll = 0;                   // roll from FC
+static int16_t      osd_pitch = 0;                  // pitch from FC
+static int16_t      osd_yaw = 0;                    // relative heading form FC
+static uint16_t     osd_throttle = 0;               // throttle*/
+
+// OpenPilot UAVTalk:
+//static uint8_t      op_uavtalk_mode = 0;            // OP UAVTalk mode, start with normal behavior
+//static uint8_t      op_alarm = 0;                   // OP alarm info
+//static uint8_t      osd_armed = 0;                  // OP armed info
+
+
