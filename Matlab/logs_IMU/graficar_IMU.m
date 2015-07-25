09ffe4fe8e2d6bde4fc79c99f8e8697d9d4b19ef
Federico Favaro <federicofsv@gmail.com>
2015-07-25 17:54:41 -0300

    logs

--- Matlab/logs_IMU/graficar_IMU.m
+++ Matlab/logs_IMU/graficar_IMU.m
@@ -19,20 +19,21 @@ clc
 % 14 pos.z 
 % 15 yaw_d
 % 16 u_yaw
-% 17 T_s_IMU
-% 18 T_us_IMU
-% 19 alt
-% 20 us_obs
-% 21 us_alt
+% 17 h_d
+% 18 u_h
+% 19 T_s_IMU
+% 20 T_us_IMU
+% 21 alt
+% 22 us_obs
+% 23 us_alt
 
 
 %% Grafica datos obtenidos
-data = importdata('log_todo_colgado_2',' ');
-% dataB = importdata('log_PD_nuevo_3',' ');
+data = importdata('log_todo_colgado_5',' ');
 
 t_CC3D = data(:,1)*1000 + data(:,2)/1000; %timepo CC3D en milisegundos
 t_main = data(:,10)*1000 + data(:,11)/1000; %timepo main en milisegundos
-t_IMU = data(:,17)*1000 + data(:,18)/1000; %timepo main en milisegundos
+t_IMU = data(:,19)*1000 + data(:,20)/1000; %timepo main en milisegundos
 
 display('promedio de tiempo del main:');
 display(mean(diff(t_main(162:length(t_main)))));
@@ -55,23 +56,37 @@ legend('roll(deg)','pitch(deg)','yaw(deg)')
 grid on
 hold off
 
-
 figure
-plot(t_main,data(:,19))
-title('Altura')
+plot(t_main,data(:,17))
+title('Altura deseada')
 xlabel('t(ms)')
 ylabel('h(m)')
 grid on
 
 figure
-plot(t_main,data(:,20))
+plot(t_main,data(:,18))
+title('senal de control altura')
+xlabel('t(ms)')
+ylabel('h(m)')
+grid on
+
+
+figure
+plot(t_main,data(:,21))
+title('Altura baro')
+xlabel('t(ms)')
+ylabel('h(m)')
+grid on
+
+figure
+plot(t_main,data(:,22))
 title('Distancia us frente')
 xlabel('t(ms)')
 ylabel('d(m)')
 grid on
 
 figure
-plot(t_main,data(:,21))
+plot(t_main,data(:,23))
 title('Distancia us altura')
 xlabel('t(ms)')
 ylabel('d(m)')
